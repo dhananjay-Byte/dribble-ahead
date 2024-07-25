386d894d7323fdbb06033b4111675f5971860f6c
Dhananjay <82323909+dhananjay-Byte@users.noreply.github.com>
2024-07-25 22:56:38 +0530

    Update Improvement.tsx

--- src/app/Components/Improvement.tsx
+++ src/app/Components/Improvement.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Improvement() {
   return (
     <div id="manifesto" className='min-h-screen p-8 md:p-16 lg:p-32'>
-      <h3 className='font-medium text-lg md:text-xl lg:text-2xl mb-4'>Wrong with self-improvement & how we're fixing it.</h3>
+      <h3 className='font-medium text-lg md:text-xl lg:text-2xl mb-4'>Wrong with self-improvement and how we are fixing it.</h3>
       <h1 className='font-bold text-3xl md:text-4xl lg:text-5xl mb-12'>Self-Improvement. Ugh.</h1>
       <div className='flex flex-col gap-8 lg:ml-32 h-[55vh] overflow-y-scroll pr-4'>
         {Array.from({ length: 6 }).map((_, index) => (
